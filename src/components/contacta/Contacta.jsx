b10310bf7a9eef2cee7ca7f258ec58473cafb2a7
albinramos <dramosh79@gmail.com>
2024-01-16 20:25:59 +0100

    popup contactar y button back to perfil

--- src/components/contacta/Contacta.jsx
+++ src/components/contacta/Contacta.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useRef, useEffect} from "react";
 import { Navigate, useNavigate } from 'react-router-dom';
+import { IoMdArrowRoundBack } from "react-icons/io";
 import "./contacta.css"
 
 const Contacta = () => {
+
+  const navigate = useNavigate();
+
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
     useEffect (() => {
       fetchApi();
@@ -33,9 +38,6 @@ const Contacta = () => {
     });
     console.log("contacta",fetchData);
 
-    const navigate = useNavigate();
-  
-
     const handleSubmit = async (e) => {
       e.preventDefault();
       
@@ -50,7 +52,8 @@ const Contacta = () => {
       })
       if (response.ok) {
         console.log("mensaje enviado con exito");
-        setIsFormSubmitted(true);
+        //setIsFormSubmitted(true);
+        setIsPopupVisible(true);
       } else {
         console.error("Error en envío de mensaje");
       }
@@ -60,10 +63,16 @@ const Contacta = () => {
       }
     }
 
+    const handlePopupClose = () => {
+      setIsPopupVisible(false);
+      navigate('/');
+    }
+
   return(
     <>
       <section className='main-container-contacta'>
       <div className="contacta-first-div">
+        {/* <span className="contacta-icon-span"><IoMdArrowRoundBack></IoMdArrowRoundBack></span> */}
         <div className="contacta-first-div-text">
           <p className="contacta-p-name">{fetchData.userData.firstname}<br></br>
           </p>
@@ -81,6 +90,15 @@ const Contacta = () => {
             <p className="contacta-p-contacta-3">Recuerda que todos los mensajes son anónimos. En caso de que quieras personalizar tu mensaje, añade tu nombre, apellido y departamento. </p>
             <button className="button-contacta" >Enviar</button>
           </form>
+          {isPopupVisible && (
+            <div className="popup-container">
+              <div className="popup">
+                <img src="../../src/assets/logo-company.png" alt="logo empresa" className="popup-company-logo" />
+                <p className="p-popup">¡Tu mensaje se ha enviado con éxito!</p>
+                <button onClick={handlePopupClose} className="popup-button">Volver al perfil</button>
+              </div>
+            </div>
+          )}
         </div>
       </section>
     </>
